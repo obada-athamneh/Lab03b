e3c00c3a267ba36426b429d33d8c7e3d2b3515ef
Obada Athamneh <obadaobaida@gmail.com>
2021-01-27 16:18:09 +0200

    Made by deve

---
+++ lab04.js
@@ -0,0 +1,19 @@
+var color = prompt('What color do you prefer');
+var username = prompt('What is your name');
+alert ('Welcome' + ' ' + username);
+document.write(color + ' is nice')
+var age = prompt('what your age') 
+if (age >= 40 ) {
+document.write( '<h2>' + 'you are oldest' + '</h2>' ) }
+else if (20 < age < 40) {
+    document.write( '<h2>' + 'you are yonger' + '</h2>' )
+}
+     else {alert( 'you are baby' )}
+
+var X = 10;
+var B = 11;
+console.log(X + B);
+
+var t;
+var r = confirm("Press a button!");
+
